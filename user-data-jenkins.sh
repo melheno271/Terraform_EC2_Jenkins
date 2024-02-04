bb4d34919f15438a1c5be47629af8cf17a002bbd
melheno271 <70054092+melheno271@users.noreply.github.com>
2024-02-03 21:31:53 -0800

    Update user-data-jenkins.sh

--- user-data-jenkins.sh
+++ user-data-jenkins.sh
@@ -7,6 +7,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-17-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+# https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
